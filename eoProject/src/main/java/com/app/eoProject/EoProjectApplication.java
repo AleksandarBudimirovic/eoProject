e46f1b3505a4ebabb9ff7431ccdfd6b198edfe8a
Aleksandar Budimirovic <aleksandar.budimirovic97@gmail.com>
2021-03-02 11:38:43 +0100

    init

---
+++ eoProject/src/main/java/com/app/eoProject/EoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.app.eoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EoProjectApplication.class, args);
+	}
+
+}
---
+++ eoProject/src/test/java/com/app/eoProject/EoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.eoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
