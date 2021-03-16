2de6618dd6d87e4c765d1dba94998784509e5194
aleksandra49 <anja.9811@gmail.com>
2021-03-16 16:52:45 +0100

    Uradjen do pola service za klasu enrolment #6

--- eoProject/src/main/java/com/app/eoProject/service/EnrollmentService.java
+++ eoProject/src/main/java/com/app/eoProject/service/EnrollmentService.java
@@ -1,8 +1,35 @@
 package com.app.eoProject.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.app.eoProject.model.Enrollment;
+import com.app.eoProject.repository.EnrollmentRepository;
+
+
 
 @Service
 public class EnrollmentService {
+	
+	@Autowired
+	EnrollmentRepository enrollmentRepository;
+	
+	public Enrollment findOne(Long id) {
+		return enrollmentRepository.findById(id).orElse(null);
+	}
+
+	public List<Enrollment> findAll() {
+		return enrollmentRepository.findAll();
+	}
+	
+	public Enrollment save(Enrollment enrollment) {
+		return enrollmentRepository.save(enrollment);
+	}
+
+	public void remove(Long id) {
+		enrollmentRepository.deleteById(id);
+	}
 
 }
